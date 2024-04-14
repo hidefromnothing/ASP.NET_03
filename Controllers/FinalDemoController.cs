6060e4e1e5bf0ad27035c1e4c09c237d6b4c6821
hidefromnothing <115701715+hidefromnothing@users.noreply.github.com>
2024-04-14 19:44:00 -0400

    Add files via upload

---
+++ Controllers/FinalDemoController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FinalReview.Controllers
+{
+    public class FinalDemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // About us => About
+        public IActionResult About()   // About.cshtml
+        {
+            return View();
+        }
+
+        // Contact us => Contact
+        public IActionResult Contact()  // Contact.cshtml
+        {
+            return View();
+        }
+
+        // List of products
+       
+
+
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FinalReview.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
